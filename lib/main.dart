6ec20421e9fab7be6c4c193a0ca13c41330ab3c9
mohab mohamed <mohapmohamed293@gmai.com>
2025-02-21 13:16:29 +0200

    create notes item

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,11 @@ class NotesApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           brightness: Brightness.dark,
+          fontFamily: 'Poppins'
 
         ),
         home: const NotesView(),
+
 
       );
   }
