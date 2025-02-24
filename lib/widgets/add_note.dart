b2eab418c628de49559288bc8676e2626b98d276
mohab mohamed <mohapmohamed293@gmai.com>
2025-02-24 11:24:43 +0200

    add fields to custom text field

---
+++ lib/widgets/add_note.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
+
+class AddNote extends StatelessWidget {
+  const AddNote({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            hint: 'title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hint: 'content',
+            maxLines: 5,
+          ),
+
+        ],
+      ),
+    );
+  }
+}
