ea4d67e06cc40e0c5601bb4ce4eefdaace415964
mohab mohamed <mohapmohamed293@gmai.com>
2025-02-28 14:42:02 +0200

    fix issue of scroll of text field and the issue of loading indicato

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,24 +1,34 @@
 
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/notes_view_body.dart';
+
+
 
 import '../widgets/add_note.dart';
+import '../widgets/notes_view_body.dart';
+
+
 class NotesView extends StatelessWidget {
-  const NotesView({super.key});
+  const NotesView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: ()
-      {
-        showModalBottomSheet(context: context, builder: (context)
-        {
-            return AddNoteBottomSheet();
-        });
-      },
-        child:const Icon(Icons.add) ,),
+      resizeToAvoidBottomInset: false,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              });
+        },
+        child: const Icon(Icons.add),
+      ),
       body: const NotesViewBody(),
     );
   }
 }
-
