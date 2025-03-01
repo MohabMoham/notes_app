50b628e0b9ccd9285f0a35a5b80024eb198632e6
mohab mohamed <mohapmohamed293@gmai.com>
2025-03-01 13:39:49 +0200

    edit note done

--- lib/widgets/add_note.dart
+++ lib/widgets/add_note.dart
@@ -4,6 +4,7 @@ import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 
+import '../cubits/notes_cubit/notes_cubit.dart';
 import 'add_note_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -18,7 +19,7 @@ class AddNoteBottomSheet extends StatelessWidget {
           if (state is AddNoteFailure) {}
 
           if (state is AddNoteSuccess) {
-
+            BlocProvider.of<NotesCubit>(context).fetchAllNotes();
             Navigator.pop(context);
           }
         },
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,30 +1,39 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint,  this.maxLines = 1, this.onSaved});
-final String hint;
-final int maxLines;
+  const CustomTextField(
+      {super.key,
+        required this.hint,
+        this.maxLines = 1,
+        this.onSaved,
+        this.onChanged});
+
+  final String hint;
+  final int maxLines;
+
   final void Function(String?)? onSaved;
+
+  final Function(String)? onChanged;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onChanged: onChanged,
       onSaved: onSaved,
-      validator: (value)
-      {
-        if(value?.isEmpty ?? true)
-          {
-            return 'field is empty';
-          }
-        else{
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return 'Field is required ';
+        } else {
           return null;
         }
       },
-      cursorColor:kPrimaryColor ,
+      cursorColor: kPrimaryColor,
       maxLines: maxLines,
       decoration: InputDecoration(
         hintText: hint,
-
         border: buildBorder(),
         enabledBorder: buildBorder(),
         focusedBorder: buildBorder(kPrimaryColor),
@@ -34,8 +43,11 @@ final int maxLines;
 
   OutlineInputBorder buildBorder([color]) {
     return OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: BorderSide(
-              color: color?? Colors.white));
+        borderRadius: BorderRadius.circular(
+          8,
+        ),
+        borderSide: BorderSide(
+          color: color ?? Colors.white,
+        ));
   }
 }
